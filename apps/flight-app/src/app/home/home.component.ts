28c62ef42ea40fc6019d32cc5ea9e3af72877218
Alex Thalhammer <alex@angular-software.at>
2021-11-12 23:06:33 +0100

    Updates & improvements

--- apps/flight-app/src/app/home/home.component.ts
+++ apps/flight-app/src/app/home/home.component.ts
@@ -13,24 +13,24 @@ import { map } from 'rxjs/operators';
 export class HomeComponent implements OnInit {
   expertMode = false;
   needsLogin$: Observable<boolean>;
+
+  constructor(private route: ActivatedRoute) {}
+
   _userName = '';
 
   get userName(): string {
     return this._userName;
   }
 
-  constructor(private route: ActivatedRoute) {}
-
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   changed($event): void {
     console.debug('$event.detail ', $event.target.detail);
 
     this.expertMode = $event.detail;
   }
 
-  ngOnInit() {
-    this.needsLogin$ = this.route.params.pipe(
-      map(params => !!params['needsLogin'])
-    );
+  ngOnInit(): void {
+    this.needsLogin$ = this.route.params.pipe(map((params) => !!params['needsLogin']));
   }
 
   login(): void {
